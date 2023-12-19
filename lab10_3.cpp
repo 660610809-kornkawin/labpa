1799c9a0d37a5b951ec8010d25dcf2b8796c07ae
660610809-kornkawin <kornkawin_th@cmu.ac.th>
2023-12-19 16:23:30 +0700

    lab10-3-completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+#include<fstream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+
+int i = 0;
+double sum = 0 ;
+double sum_of_square = 0;
+string textline;
+ifstream source("score.txt");
+while (getline(source,textline))
+{
+    sum += atof(textline.c_str());
+    sum_of_square += pow(atof(textline.c_str()),2);
+    i++;
+}
+
+cout << "Number of data = "<<i<<"\n";
+cout << setprecision(3);
+cout << "Mean = "<<sum/i<<"\n";
+cout << "Standard deviation = "<<sqrt((sum_of_square/i)-pow(sum/i,2));
+ return 0;
+}
